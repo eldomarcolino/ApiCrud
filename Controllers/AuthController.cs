d4e58aac78f8b18d2fa3b07da007784fd015c848
eldomarcolino <eldoedaniel@gmail.com>
2025-02-19 23:44:15 -0400

    teste com token

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaDeRecarga.Auth;
+using SistemaDeRecarga.Model;
+
+namespace SistemaDeRecarga.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly TokenService _tokenService;
+
+        public AuthController(TokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] User user)
+        {
+            // Simulação de autenticação (substituir por uma lógica real)
+            if (user.UserName == "admin" && user.Password == "admin123")
+            {
+                var token = _tokenService.GenerateToken(user);
+                return Ok(new { token });
+            }
+
+            return Unauthorized("Usuário ou senha inválidos.");
+        }
+    }
+}
